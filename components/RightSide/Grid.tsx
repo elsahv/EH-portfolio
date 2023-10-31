1442bf7710916f489b87c54622039413149e0f53
elsahv <elsahvy@gmail.com>
2023-10-31 16:36:13 -0700

    grid progress

--- components/RightSide/Grid.tsx
+++ components/RightSide/Grid.tsx
@@ -4,7 +4,7 @@ import Img2 from '../../public/screenshots/rq.png'
 import Img3 from '../../public/screenshots/wtbh.png'
 import Link from 'next/link'
 import { MdFileOpen } from "react-icons/md";
-import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const posts = [
   {
@@ -12,19 +12,22 @@ const posts = [
     title: 'small businesses',
     image: Img1,
     url: 'http://laughingcoyote.dev',
-    p: 'Laughing coyote- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure porro, dolor voluptatem blanditiis expedita tempora ab',
+    p: 'Laughing coyote- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure porro, dolor voluptatem blanditiis t amet consectetur adipisicing elit. Iure porro, dolor voluptatem blanditiis expedita tempora ab',
+    github:"https://github.com/elsahv/laughing-coyote-dev"
   },
   {
     title: 'high desert locals and enthusiasts',
       image: Img2,
       url: 'http://therunningquail.com',
-      p: 'The Running Quail- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure porro, dolor voluptatem blanditiis expedita tempora ab',
+      p: 'The Running Quail- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure porro, dolor voluptatem blanditiis t amet consectetur adipisicing elit. Iure porro, dolor voluptatem blanditiis expedita tempora ab',
+      github:"https://github.com/elsahv/-the-running-quail"
   },
   {
     title: 'and dietitians/ nutritionists',
     image: Img3,
     url: 'http://writingtobetterhealth.com',
-    p: 'Writing to Better Health- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure porro, dolor voluptatem blanditiis expedita tempora ab',
+    p: 'Writing to Better Health- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure porro, dolor voluptatem blanditiis t amet consectetur adipisicing elit. Iure porro, dolor voluptatem blanditiis expedita tempora ab',
+    github:"https://github.com/elsahv/writing-to-better-health"
   },
 ]
 
@@ -54,37 +57,44 @@ const Grid = () => {
         <div className="">
       <p className="text-lg">Listed below are some featured websites. These projects are build to assist:</p>
     
-         {/* <ul className="pl-5">
-          <li>- small businesses</li>
-          <li>- high desert residents</li>
-          <li>- dietitians</li>
-         </ul> */}
+    
          </div>
       {/* //*GRID STARTS HERE */}
     <div className="grid grid-cols-1 gap-x-4 gap-y-12">
       {posts.map((item, index) => (
         <div className="rounded dropshadow-lg" key={index}>
-           <Link className="hover:opacity-80 hover:ease-in duration-300" href={item.url}>
+
+            {/* SQ STARTS HERE */}
             <div className="2xl:flex 2xl:flex-row flex-col bg-test2 border border-black rounded-lg">
-              <Image className="2xl:w-[550px] w-full rounded-tl-lg rounded-bl-lg drop-shadow-3xl border-r border-onyx"
+           <Link className="hover:opacity-80 hover:ease-in duration-300" href={item.url}>
+              <Image className="2xl:w-[1250px] 2xl:h-[250px] w-full rounded-tl-lg rounded-bl-lg drop-shadow-3xl border-r border-onyx"
                     src={item.image}
                     alt={item.title}
                     />
+                 </Link>
                     <div className="">
             <h3 className="text-lg font-bold pt-5 pl-5">...{item.title}</h3>
             <p className="">{item.p}</p>
-            <div className="pt-5 pl-5 2xl:pb-0 pb-4 flex items-center h-1/2 md:text-3xl text-2xl">
-              {/* //todo IMP LINKS */}
-              <div className="px-1">
-                <FaLinkedin />
-                </div>
-              <div className="px-1">
-                <MdFileOpen />
-                </div>
-            </div>
+                      <>
+                                <div id="sm-links" className='text-2xl flex 2xl:ml-5 ml-2 mb-5 2xl:mb-1'>
+                                  <div className="pl-2">
+                                  <Link className="hover:opacity-80 hover:ease-in duration-300" href={item.url}>
+                                    <MdFileOpen />
+                                   </Link>
+                                    </div>
+                                        <div className="pl-2">
+                                        <Link className="hover:opacity-80 hover:ease-in duration-300" href={item.github}>
+                                    <FaGithub />
+                                    </Link>
+                                    </div>
+                                
+                                
+                                </div>
+                      </>
                     </div>
                     </div>
-                 </Link>
+            {/* SQ ENDS HERE */}
+
         </div>
       ))}
     </div>
